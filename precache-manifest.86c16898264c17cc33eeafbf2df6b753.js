bdd5057f272e1a62a3f866df835d1fa70bc0b056
ukabyl <umkabylbekov@gmail.com>
2020-04-08 14:06:52 +0500

    Updates

---
+++ precache-manifest.86c16898264c17cc33eeafbf2df6b753.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9fac855ef97c2b969079079062873e7a",
+    "url": "/star-db/index.html"
+  },
+  {
+    "revision": "8d293957d734243bce49",
+    "url": "/star-db/static/css/main.416d03bf.chunk.css"
+  },
+  {
+    "revision": "5d8c19dc738fca03143e",
+    "url": "/star-db/static/js/2.f82cdb4e.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/star-db/static/js/2.f82cdb4e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d293957d734243bce49",
+    "url": "/star-db/static/js/main.63f1d564.chunk.js"
+  },
+  {
+    "revision": "ba152e2bbb51542a9c05",
+    "url": "/star-db/static/js/runtime-main.e01fc811.js"
+  }
+]);
